754bae81dc4b35e147b831cc60a7af99ba15103f
GodSamomen <71833103+GodSamomen@users.noreply.github.com>
2021-01-22 16:49:39 +0300

    Spammer

---
+++ Spammer.py
@@ -0,0 +1,23 @@
+from pynput.keyboard import Key, Controller
+import time, math
+
+message = input('Gönderilecek Mesaj:')
+count = int(input('Gönderilecek Mesaj Sayısı:'))
+
+for i in range(5):
+	print(str(5 - i))
+	time.sleep(1)
+
+keyboard = Controller()
+waitTime = 0.01 * len(message)
+def pressKey(key):
+	keyboard.press(key)
+	keyboard.release(key)
+
+for i in range(count):
+	progress = math.floor(i / count * 100)
+	keyboard.type(message)
+	pressKey(Key.enter)
+	time.sleep(waitTime)
+
+print('Gönderildi')
